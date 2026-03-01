b3976b646cc32e712fe8607bd94d5effe4c2e73a
PRANESH-MANICAKM <praneshappu2001@gmail.com>
2026-03-02 00:44:13 +0530

    max sum

--- array/max-sub-array.js
+++ array/max-sub-array.js
@@ -5,7 +5,7 @@ let arr = [-2, -3, 4, -1, -2, 1, 5, -3];
 const maxSumArray = (arr) => {
     let maxSum = arr[0];
     let currentSum = arr[0];
-    for(let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         currentSum = Math.max(arr[i], currentSum + arr[i]);
         maxSum = Math.max(currentSum, maxSum);
     }
@@ -15,3 +15,25 @@ const maxSumArray = (arr) => {
 console.time("1");
 console.log(maxSumArray(arr));
 console.timeEnd("1");
+
+const maxSum = (arr) => {
+    let currentSum = arr[0];
+    let maxSum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > currentSum + arr[i]) {
+            currentSum = arr[i];
+        } else {
+            currentSum = currentSum + arr[i]
+        }
+
+        if (currentSum > maxSum) {
+            maxSum = currentSum
+        }
+    }
+
+    return maxSum;
+};
+
+console.time("2");
+console.log(maxSum(arr));
+console.timeEnd("2");
